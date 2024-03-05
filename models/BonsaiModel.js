d0034e603fa2533e2b1c1e76748b06500f9214a4
BeatrizMercado <bea.mercado.lop@gmail.com>
2024-03-05 12:17:18 +0100

    add bonsai model table

---
+++ models/BonsaiModel.js
@@ -0,0 +1,30 @@
+import { Sequelize, DataTypes, Model } from "sequelize";
+
+const Bonsai = sequelize.define('bonsais', {
+    // Model attributes are defined here
+    specie: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    abonated: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    trasplanted: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+      },
+    notes: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    images: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+  }, {
+    // Other model options go here
+  });
+  
+  // `sequelize.define` also returns the model
+  console.log(Bonsai === sequelize.models.Bonsai); // true
