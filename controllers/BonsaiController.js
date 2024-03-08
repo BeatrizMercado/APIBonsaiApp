81e805a2dcfffc7f1467847924648a8e3801fb85
SarayAnta <s.rodriguezanta@gmail.com>
2024-03-08 12:27:44 +0100

    add delete method

--- controllers/BonsaiController.js
+++ controllers/BonsaiController.js
@@ -11,4 +11,15 @@ export const getAllBonsais = async (request, response) => {
     }
 }
 
+export const deleteBonsai = async (request, response) => {
+
+    try {
+        const { id } =request.params;
+        const bonsai = await BonsaiModel.destroy({ where: { id }});
+        response.status(200).json(bonsai);
+    } catch (error) {
+        response.status(500).json({ message: error.message });
+    }
+}
+
 
--- routes/BonsaiRouter.js
+++ routes/BonsaiRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getAllBonsais } from "../controllers/BonsaiController.js";
+import { deleteBonsai } from "../controllers/BonsaiController.js";
 
 const router = express.Router();
 
 router.get("/", getAllBonsais);
+router.delete("/:id", deleteBonsai);
 
 export default router
