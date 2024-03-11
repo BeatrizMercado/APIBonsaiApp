97ceaa74e9184181f392156b5ba15e0c12ba77f2
SarayAnta <s.rodriguezanta@gmail.com>
2024-03-11 11:16:41 +0100

    add put method

--- controllers/BonsaiController.js
+++ controllers/BonsaiController.js
@@ -24,15 +24,34 @@ export const deleteBonsai = async (request, response) => {
 
 export const createBonsai = async (request, response) => {
     try {
-        const newBonsai = await BonsaiModel.create({
-            abonated: request.body.abonated,
-            specie: request.body.specie,
-            trasplanted: request.body.trasplanted,
-            notes: request.body.notes,
-            images: request.body.images
-        });
+        const newBonsai = await BonsaiModel.create(
+            request.body
+        );
 
         response.status(201).json(newBonsai);
+    } catch (error) {
+        response.status(500).json({ message: error.message });
+    }
+};
+
+export const updateBonsai = async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const existingBonsai = await BonsaiModel.findByPk(id);
+        if (!existingBonsai) {
+            return response.status(404).json({ message: 'Bonsai not found' });
+        }
+
+        await BonsaiModel.update(
+            request.body,
+            { where: { id } }
+        );
+
+
+        const updatedBonsai = await BonsaiModel.findByPk(id);
+
+        response.status(201).json({ message: 'Bonsai updated successfully', bonsai: updatedBonsai });
     } catch (error) {
         response.status(500).json({ message: error.message });
     }
