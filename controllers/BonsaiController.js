005241ee68269c132e5b268084718375aa1b1a10
rebecavm28 <rebe14@hotmail.es>
2024-03-11 13:40:34 +0100

    metodo-update

--- controllers/BonsaiController.js
+++ controllers/BonsaiController.js
@@ -1,6 +1,6 @@
 import BonsaiModel from "../models/BonsaiModel.js"
 
-export const getAllBonsais = async (request, response) => {
+export const getAllBonsais = async (request, response) => {   //metodo get//
 
     try {
         const bonsais =  await BonsaiModel.findAll();
@@ -11,7 +11,7 @@ export const getAllBonsais = async (request, response) => {
     }
 }
 
-export const deleteBonsai = async (request, response) => {
+export const deleteBonsai = async (request, response) => {  //metodo delete//
 
     try {
         const { id } =request.params;
@@ -22,7 +22,7 @@ export const deleteBonsai = async (request, response) => {
     }
 }
 
-export const createBonsai = async (request, response) => {
+export const createBonsai = async (request, response) => { //metodo post//
     try {
         const newBonsai = await BonsaiModel.create({
             abonated: request.body.abonated,
@@ -37,3 +37,29 @@ export const createBonsai = async (request, response) => {
         response.status(500).json({ message: error.message });
     }
 };
+
+//update//
+export const updateBonsai = async (request, response) => {
+  try {
+    // Extrae el ID del parámetro de la solicitud
+    const bonsaiId = request.params.id;
+
+    await BonsaiModel.update(request.body,{where:{id:bonsaiId}});
+    
+    // Busca el bonsai por ID
+    //const bonsai = await BonsaiModel.findByPk(bonsaiId);
+
+    // Si no se encuentra el bonsai, devuelve un error 404
+    if (!bonsai) {
+      return response.status(404).json({ message: 'Bonsai no encontrado.' });
+    }
+
+    // Responde con el bonsai actualizado
+    response.status(201).json(bonsai);
+  } catch (error) {
+    // Maneja errores y responde con un código de estado 500 en caso de error interno
+    response.status(500).json({ message: error.message });
+  }
+};
+
+//metodo getbyId//
