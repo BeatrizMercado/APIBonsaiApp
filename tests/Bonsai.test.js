14a9ea75a770ed6913c5b7ea0ec071c58bd0ad67
BeatrizMercado <bea.mercado.lop@gmail.com>
2024-03-13 15:19:09 +0100

    add afterAll to delete data after testing

--- tests/Bonsai.test.js
+++ tests/Bonsai.test.js
@@ -1,9 +1,15 @@
 import request from 'supertest';
-import app from '../app.js'
+import app from '../app.js';
+import BonsaiModel from '../models/BonsaiModel.js';
+
 
 const api = request(app); //request nos permite hacer solicitudes a la app
 
 describe('Testing CRUD bonsais', ()=> {
+
+    afterAll(async () => {
+        await BonsaiModel.destroy({where: {specie: "test"}})
+    });
 
     test('Response body must be an array and then show 200 status', async() =>{
     const response = await api.get('/api');//despues de hacer la petición get...
@@ -13,14 +19,16 @@ describe('Testing CRUD bonsais', ()=> {
 
     test('Post response should be an object and return status 201', async ()=> {
         const response = await api.post('/api').send({
-            "specie": "test",
-            "abonated": "2020-02-02",
-            "trasplanted": "2020-02-02",
-            "notes": "test",
-            "images": "https://damiandeluca.com"
+            specie: "test",
+            abonated: "2020-02-02",
+            trasplanted: "2020-02-02",
+            notes: "test",
+            images: "https://damiandeluca.com"
         });
+
         expect(typeof response.body).toBe('object');
         expect(response.status).toBe(201);
     })
+
 })
 
