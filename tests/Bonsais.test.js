837b80cad165c3b8323018ca94378d23c1c9f165
rebecavm28 <rebe14@hotmail.es>
2024-03-18 10:46:56 +0100

    delete test

--- tests/Bonsais.test.js
+++ tests/Bonsais.test.js
@@ -6,7 +6,7 @@ const api = request(app);
 
 describe('Testing CRUD Bonsais',()=>{
 
-test('aqui pones lo que quieres testear para ti', async()=>{
+test('should return status 201 and response should be an object', async()=>{
 const response = await api.get('/api');
 expect(Array.isArray(response.body)).toBe(true);
 expect(response.status).toBe(200);
@@ -28,3 +28,15 @@ test('Post response should be an object and return status 201', async ()=> {
 
 })
 
+test('should return status 200 and response should be an object', async () => {
+    // Supongamos que tienes un identificador de un recurso existente en tu base de datos
+    const resourceId = '3';
+
+    // Realiza la solicitud DELETE a la ruta correspondiente con el identificador del recurso
+    const response = await api.delete(`/api/${resourceId}`);
+
+    // Verifica que la respuesta tenga el código de estado 200 (OK)
+    expect(response.status).toBe(200);
+
+   
+})
